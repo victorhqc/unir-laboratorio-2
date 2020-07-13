297b13e62e4fba7a56136e528fe63ce33450a443
Victor Quiroz <victorhqc@gmail.com>
2020-07-13 19:00:11 +0200

    iniciando validación

--- PedidosFarmacia/MainWindow.xaml.cs
+++ PedidosFarmacia/MainWindow.xaml.cs
@@ -27,12 +27,56 @@ namespace PedidosFarmacia
 
         private void AceptarButton_Click(object sender, RoutedEventArgs e)
         {
+            if (!this.Validate_Form())
+            {
+                return;
+            }
             Console.WriteLine("Hello Aceptar");
         }
 
         private void CancelarButton_Click(object sender, RoutedEventArgs e)
         {
             Console.WriteLine("Hello Cancelar");
+        }
+
+        private bool Validate_Form()
+        {
+            bool isValid = true;
+            ErrorTextBlock.Visibility = Visibility.Hidden;
+
+            if (!this.Validate_Name())
+            {
+                isValid = false;
+                this.show_error("El nombre es incorrecto, porfavor revisa que hayas ingresado un nombre al medicamento.");
+            }
+
+            if (isValid && !this.Validate_Provider())
+            {
+                isValid = false;
+                this.show_error("Porfavor, Elige un proveedor.");
+            }
+
+            return isValid;
+        }
+
+        private bool Validate_Name()
+        {
+            string value = NombreTextBox.Text;
+
+            return value.Length > 0;
+        }
+
+        private bool Validate_Provider()
+        {
+            ComboBoxItem item = (ComboBoxItem)MedicamentoComboBox.SelectedItem;
+
+            return item != null;
+        }
+
+        private void show_error(string e)
+        {
+            ErrorTextBlock.Visibility = Visibility.Visible;
+            ErrorTextBlock.Text = e;
         }
     }
 }
