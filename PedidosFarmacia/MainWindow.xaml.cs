b7002bce5f4e8aadce8f9e9f42e36aa16a19fb68
Victor Quiroz <victorhqc@gmail.com>
2020-07-13 19:21:36 +0200

    validacion

--- PedidosFarmacia/MainWindow.xaml.cs
+++ PedidosFarmacia/MainWindow.xaml.cs
@@ -53,7 +53,25 @@ namespace PedidosFarmacia
             if (isValid && !this.Validate_Provider())
             {
                 isValid = false;
-                this.show_error("Porfavor, Elige un proveedor.");
+                this.show_error("Por favor, elige un tipo de medicamento.");
+            }
+
+            if (isValid && !this.Validate_Amount())
+            {
+                isValid = false;
+                this.show_error("La cantidad ingresada debe ser una cantidad positiva, y no debe tener decimales.");
+            }
+
+            if (isValid && !this.Validate_Distributor())
+            {
+                isValid = false;
+                this.show_error("Por favor, elige un distribuidor");
+            }
+
+            if (isValid && !this.Validate_Branch())
+            {
+                isValid = false;
+                this.show_error("Por favor, elige una sucursal");
             }
 
             return isValid;
@@ -71,6 +89,33 @@ namespace PedidosFarmacia
             ComboBoxItem item = (ComboBoxItem)MedicamentoComboBox.SelectedItem;
 
             return item != null;
+        }
+
+        private bool Validate_Amount()
+        {
+            try
+            {
+                string value = CantidadTextBox.Text;
+                int num = Int32.Parse(value);
+
+                return num > 0;
+            } catch(FormatException)
+            {
+                return false;
+            }
+        }
+
+        private bool Validate_Distributor()
+        {
+            return CofarmaRadioButton.IsChecked == true
+                || EmpsepharRadioButton.IsChecked == true
+                || CemefarRadioButton.IsChecked == true;
+        }
+
+        private bool Validate_Branch()
+        {
+            return PrincipalCheckBox.IsChecked == true
+                || SecundariaCheckBox.IsChecked == true;
         }
 
         private void show_error(string e)
